69ae41c31b4fa710bccf32e4d0db37fef5c5b94a
Art <artjohanaaspollu@gmail.com>
2023-07-29 18:32:41 +0300

    created all db tables

--- backend/database/migration/000009_create_exclusive_posts_table.up.sql
+++ backend/database/migration/000009_create_exclusive_posts_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS exclusive_posts (
     postId INTEGER NOT NULL,
     selectedUserId INTEGER NOT NULL,
     FOREIGN KEY (postId) REFERENCES posts (postId) ON DELETE CASCADE,
-    FOREIGN KEY (selectedUserId) REFERENCES users (userId) ON DELETE CASCADE,
+    FOREIGN KEY (selectedUserId) REFERENCES users (userId) ON DELETE CASCADE
 );
