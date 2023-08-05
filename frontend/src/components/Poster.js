91c78007a8a646c209443bcab47008611d349009
oyarukhi <olga.yarukhina@gmail.com>
2023-08-05 19:11:55 +0300

    start with privacy

--- frontend/src/components/Poster.js
+++ frontend/src/components/Poster.js
@@ -6,8 +6,15 @@ const CreatePost = ({ userId }) => {
         content: "",
     });
     const [selectedImg, setSelectedImg] = useState(null);
+    const [isChecked, setIsChecked] = useState(false);
     // const [uploaded, setUploaded] = useState();
     const imgPicker = useRef(null);
+    const textAreaRef = useRef(null);
+    const rows = 10;
+
+    const handleFocus = () => {
+        textAreaRef.current.style.height = `${rows}rem`;
+    };
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -29,6 +36,10 @@ const CreatePost = ({ userId }) => {
         imgPicker.current.click();
     }
 
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -38,11 +49,16 @@ const CreatePost = ({ userId }) => {
         if (selectedImg) {
             payload.append("img", selectedImg || "");
         };
+        if (isChecked) {
+            payload.append("privacy", "Private" || "Public");
+        };
 
         const options = {
             method: "POST",
             body: payload,
         };
+
+        console.log(payload)
 
         try {
             const response = await fetch(
@@ -63,36 +79,56 @@ const CreatePost = ({ userId }) => {
     return (
         <div>
             <form>
-                <div className="mb-3">
-                    <textarea
-                        className="form-control"
-                        rows="6"
-                        placeholder="What do you want you to say to this World?"
-                        id="content"
-                        name="content"
-                        type="button"
-                        value={formData.content}
-                        onChange={handleChange}
-                        maxLength="2000"
-                        required>
-                    </textarea>
-                </div>
-
-                <button
-                    className="btn btn-dark"
-                    type="button"
-                    onClick={handleImg}>IMG</button>
-                <input
-                    className="hidden"
-                    type="file"
-                    ref={imgPicker}
-                    onChange={handleChangeImg}
-                    accept="image/*, .png, .jpg, .gif"
-                />
-                <div className="mb-3">
-                    <button className="btn btn-dark" onClick={handleSubmit} type="submit">
-                        Save
-                    </button>
+                <div className=" card-header posts">
+                    <div className="mb-3">
+                        <textarea
+                            className="form-control textarea-resize"
+                            rows="1"
+                            placeholder="What do you want you to say to this World?"
+                            id="content"
+                            name="content"
+                            type="button"
+                            value={formData.content}
+                            onChange={handleChange}
+                            maxLength="2000"
+                            ref={textAreaRef} // Attach the ref to the text area
+                            onFocus={handleFocus} // Trigger handleFocus when the text area gains focus
+                            required>
+                        </textarea>
+                    </div>
+                    <div className="d-flex mb-2 align-items-center">
+                        <div className="col-2 d-flex" >
+                            <button
+                                className="btn image-button"
+                                onClick={handleImg}></button>
+                            <input
+                                className="hidden"
+                                type="file"
+                                ref={imgPicker}
+                                onChange={handleChangeImg}
+                                accept="image/*, .png, .jpg, .gif"
+                            />
+                        </div>
+                        <div className="col d-flex align-items-center" >
+                            <label className="checkbox-label">
+                                <input
+                                    type="checkbox"
+                                    checked={isChecked}
+                                    onChange={handleCheckboxChange}
+                                    className="custom-checkbox"
+                                />
+                                <span className="checkmark"></span>
+                               Private
+                            </label>
+                        </div>
+                        <div className="d-flex" >
+                        <button
+                            className="btn btn-danger"
+                            onClick={handleSubmit} type="submit"
+                        >Publish
+                        </button>
+                        </div>
+                    </div>
                 </div>
             </form>
 
