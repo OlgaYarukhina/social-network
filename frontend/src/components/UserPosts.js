41ac2005810c841768a22d3bcae59aec3fdd04de
oyarukhi <olga.yarukhina@gmail.com>
2023-08-10 22:39:11 +0300

    display posts in h p as it has to be for curr u

--- frontend/src/components/UserPosts.js
+++ frontend/src/components/UserPosts.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import SinglePost from "./SinglePost";
 
-const GetUserPosts = ({ userId }) => {
+const GetUserPosts = ({ userId, currentUserId }) => {
     const [posts, setUserPosts] = useState([]);
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const response = await fetch(
-                    `http://localhost:8080/get-user-posts?userId=${userId}`
+                    `http://localhost:8080/get-user-posts?userId=${userId}&currentUserId=${currentUserId}`
                     );
                 if (response.ok) {
                     const data = await response.json();
-                    console.log(data);
                     setUserPosts(data);
                 } else {
                     console.error("Failed to fetch posts:", response.status);
