b6b30f1d6c8a6af881b04f78b65674188ed52050
Art <artjohanaaspollu@gmail.com>
2023-09-03 10:33:38 +0000

    updated stop script

--- stop-docker.sh
+++ stop-docker.sh
@@ -4,6 +4,9 @@ echo -e "Stopping containers"
 docker-compose down
 
 echo -e "Removing images"
+
+# For some reason docker names created images differently depending on OS, removing both variants of the images just to be safe
+docker rmi social-network_frontend social-network_backend
 docker rmi social-network-frontend social-network-backend
 
 echo -e "Images removed"
